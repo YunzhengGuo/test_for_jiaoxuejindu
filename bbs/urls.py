2f416e9c5552a64350e3df2ac8f346299d5d7917
simp <simp00.163.com>
2019-04-14 14:03:11 +0800

    second

--- bbs/urls.py
+++ bbs/urls.py
@@ -3,13 +3,13 @@ from django.urls import path
 from . import views
 app_name = "bbs"
 urlpatterns = [
-    # path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('index', views.QuestionListView.as_view(), name='index'),
-    # path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # path('<int:question_id>/results/', views.results, name='results'),
-    # path('<int:question_id>/replay/', views.replay, name='replay'),
-    # path('topic/', views.TopicCreateView.as_view(), name='topic'),
-    # path('topic-update/', views.TopicUpdateView.as_view(), name='topic-update'),
+    path('<int:question_id>/replay/', views.replay, name='replay'),
+    path('topic/', views.TopicCreateView.as_view(), name='topic'),
+    path('topic-update/', views.TopicUpdateView.as_view(), name='topic-update'),
     path('my_page/', views.my_page, name='my_page'),
 
     
